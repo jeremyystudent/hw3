da12cd8bdef65d0e90dd8af52301d096890577cb
Jeremy Yiu <jeremyy@usc.edu>
2023-02-21 02:48:08 -0800

    Finished Part 2 update 3

--- stack.h
+++ stack.h
@@ -23,24 +23,24 @@ template <typename T>
 Stack<T>::Stack() : std::vector<T>(){}
 
 template <typename T>
-size_t Stack<T>::size() const{return std::vector.size();}
+size_t Stack<T>::size() const{return std::vector<T>::size();}
 
 template <typename T>
 bool Stack<T>::empty() const{return size()==0;}
 
 template <typename T>
-void Stack<T>::push(const T& item){std::vector.push_back(item);}
+void Stack<T>::push(const T& item){std::vector<T>::push_back(item);}
 
 template <typename T>
 void Stack<T>::pop(){
     if(empty()){throw std::underflow_error("No item in list");}
-    std::vector.pop_back();
+    std::vector<T>::pop_back();
 }
 
 template <typename T>
 T const & Stack<T>::top() const{
     if(empty()){throw std::underflow_error("No item in list");}
-    return std::vector.back();
+    return std::vector<T>::back();
 }
 
 
